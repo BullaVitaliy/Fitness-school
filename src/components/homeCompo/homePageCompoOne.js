2980beca84d4c62f1a04d7dd5bbfef2d817964eb
Vitaliy Bulla <vitaliybulla.job@gmail.com>
2022-02-01 00:40:26 +0200

    add folder components

---
+++ src/components/homeCompo/homePageCompoOne.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function HomeCompoOne() {
+
+
+    return(
+        <>
+            <div className="wrapper">
+                <div className="container">
+                    <p>tttt</p>
+                </div>
+            </div>
+        </>
+    );
+}
