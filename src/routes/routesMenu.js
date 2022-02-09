2a3dd198566e95df057859f4c377162ee53a8625
Vitaliy Bulla <vitaliybulla.job@gmail.com>
2022-02-09 20:56:09 +0200

    finished  menu

--- src/routes/routesMenu.js
+++ src/routes/routesMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 //STYLES
@@ -11,6 +11,8 @@ import CoursesPage from './../pages/coursesPage';
 import HomePage from './../pages/homePage';
 
 function routesMenu() {
+
+
   return (
     <BrowserRouter>
       <Routes>
@@ -25,23 +27,23 @@ function routesMenu() {
         <nav className="wrapper">
           <ul className="container">
             <li>
-              <Link className="home link " to="/">
-                <p className="menu_item first_item">Головна</p>
+              <Link className="link " to="/">
+                <p tabIndex={1} className="menu_item home">Головна</p>
               </Link>
             </li>
             <li>
-              <Link className="courses link" to="/courses">
-                <p className="menu_item first_second">Курси</p>
+              <Link className="link" to="/courses">
+                <p tabIndex={2} className="menu_item courses">Курси</p>
               </Link>
             </li>
             <li>
-              <Link className="about link" to="/about">
-                <p className="menu_item first_third">Про·нас</p>
+              <Link className="link" to="/about">
+                <p tabIndex={3} className="menu_item about">Про·нас</p>
               </Link>
             </li>
             <li>
-              <Link className="contacts link" to="/contacts">
-                <p className="menu_item last_item">Контакти</p>
+              <Link className="link" to="/contacts">
+                <p tabIndex={4} className="menu_item contacts">Контакти</p>
               </Link>
             </li>
           </ul>
